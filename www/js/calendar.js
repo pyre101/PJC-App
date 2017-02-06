ee5b27cacc4c65062d0a62b0b07057f6106bead9
absprague <ashtonsprague@mail.weber.edu>
2017-02-06 10:30:46 -0700

    Update calendar.js to fix calendar

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -1,3 +1,8 @@
-jQuery(document).ready(function () {
+$(document).on('pageshow','#cal',function(e,data){
 
+    $('#calendar').fullCalendar({
+        //any options we want here
+        height: "auto",
+        handleWindowResize: true
+    })
 });
