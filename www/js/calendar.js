1ce66f4674328a1ef0ced43f4ac7ca709d661176
absprague <ashtonsprague@mail.weber.edu>
2017-04-12 09:44:57 -0600

    Link bugs fixed

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -1,11 +1,14 @@
 jQuery(document).ready(function () {
-    /*if(e == null || data == null){
-        console.log("Error loading calendar");
-        return;
-    }*/
     $('#calendar').fullCalendar({
         //any options we want here
         height: "auto",
         handleWindowResize: true
-    })
+    });
+    var token = localStorage.getItem("token");
+    var num = token.search(",");
+    var role = token.substr(num + 1);
+    if(role == "Job Coach" || role == "Parent" || role == "Administrator"){
+        document.getElementById("footer").hidden = true;
+        document.getElementById("coachFooter").hidden = false;
+    }
 });
